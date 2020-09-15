9a3292ecf203ae35da8cabf1dab1965968358053
Anwesh43 <anweshthecool0@gmail.com>
2020-09-16 02:04:41 +0530

    adding CSCNode class

--- concsinifiedcirclesview/src/main/java/com/anwesh/uiprojects/concsinifiedcirclesview/ConcSinifiedCirclesView.kt
+++ concsinifiedcirclesview/src/main/java/com/anwesh/uiprojects/concsinifiedcirclesview/ConcSinifiedCirclesView.kt
@@ -117,4 +117,45 @@ class ConcSinifiedCirclesView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CSCNode(var i : Int , val state : State = State()) {
+
+        private var next : CSCNode? = null
+        private var prev : CSCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CSCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCSCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb  : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CSCNode {
+            var curr : CSCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
