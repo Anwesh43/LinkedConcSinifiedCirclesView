19e97ad6c0dd68a793e82a3cec09b04110682b8f
Anwesh43 <anweshthecool0@gmail.com>
2020-09-16 02:29:57 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedconcsinifiedcirclesview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedconcsinifiedcirclesview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedconcsinifiedcirclesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.concsinifiedcirclesview.ConcSinifiedCirclesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ConcSinifiedCirclesView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
