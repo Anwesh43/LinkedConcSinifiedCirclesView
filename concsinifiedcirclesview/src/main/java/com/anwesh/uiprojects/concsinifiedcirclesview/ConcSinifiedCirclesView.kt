0aec56a6b469bf71dec75e9be47d9230fac3bdff
Anwesh43 <anweshthecool0@gmail.com>
2020-09-16 01:43:47 +0530

    adding constants and libraries

--- concsinifiedcirclesview/src/main/java/com/anwesh/uiprojects/concsinifiedcirclesview/ConcSinifiedCirclesView.kt
+++ concsinifiedcirclesview/src/main/java/com/anwesh/uiprojects/concsinifiedcirclesview/ConcSinifiedCirclesView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.concsinifiedcirclesview
 /**
  * Created by anweshmishra on 16/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#009688",
+        "#FF5722",
+        "#4CAF50",
+        "#3F51B5"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f
+val sizeFactor : Float = 1.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
+
